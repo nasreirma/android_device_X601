317357e77bea2913ae44005793ed50c884df0885
nasreirma <nasreirma@gmail.com>
2017-12-09 19:03:21 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,6 @@
+cd patches_mtk && . apply-patches.sh
+cd ..
+cd ..
+cd ..
+cd ..
+. build/envsetup.sh && brunch X601
---
+++ vendorsetup.sh
@@ -0,0 +1,4 @@
+add_lunch_combo lineage_X601-eng
+add_lunch_combo lineage_X601-userdebug
+add_lunch_combo lineage_X601-user
+
